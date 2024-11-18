2ee95489e20042f17106bfbc914ee659e82394d5
ShahabAli-1 <88381344+ShahabAli-1@users.noreply.github.com>
2024-11-18 10:36:39 -0800

    vercel.json

--- job-portal-backend/index.js
+++ job-portal-backend/index.js
@@ -4,13 +4,11 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
-const path = require("path");
 dotenv.config();
 
 const authRoutes = require("./routes/auth");
 const jobRoutes = require("./routes/jobs");
 
-const __dirname = path.resolve();
 const app = express();
 
 // Middleware
@@ -41,11 +39,3 @@ app.get("/", (req, res) => {
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-app.use(express.static(path.join(__dirname, "/job-portal-frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "job-portal-frontend", "dist", "index.html")
-  );
-});
